1a0cc8a12174852bad872a7262cdd1a4dabbc787
bruce5283 <bcarrington7@gmail.com>
2022-05-19 18:20:30 -0400

    Update function.js
    Updated id

--- function.js
+++ function.js
@@ -9,7 +9,7 @@ window.function = function (userID) {
   const baseUrl = "https://script.google.com/macros/s/AKfycbwfY_dhL0hGA8uVdftJKJ7vp7-I_f49ZeRfWIe969R7vO9SQ6sal1jO3j6HtO6JKij8tw/exec";  // Please set your Web Apps URL.
 
 const para = {
-  spreadsheetId: userID,  // Please set your Google Spreadsheet ID.
+  spreadsheetId: "/1971o984Qle25gNayGzX1BoOOMm_vaF2ckv7DilXp0YE",  // Please set your Google Spreadsheet ID.
   sheetName: "Data"  // Please set the sheet name you want to retrieve the values.
 };
 const q = new URLSearchParams(para);
